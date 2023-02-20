8360298b99459bed26d9efaa570ca1024923cb99
Jamazza85 <125528436+Jamazza85@users.noreply.github.com>
2023-02-20 19:59:13 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+document.getElementById("Modo_normal").addEventListener ('click', function () {
+    document.body.style.backgroundColor = '#292929';
+    console.log("Se cambio al modo Normal.");
+});
+
+document.getElementById("Modo_psicodelico").addEventListener ('click', function () {
+    document.body.style.backgroundColor = 'pink';
+    console.log("Se cambio al modo Psicodelico.");
+    
+   
+});
